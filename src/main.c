fefd392b6954be73fb88a02e4596b02dae8fdc45
mpenon4 <penonmarcos@yahoo.com.ar>
2023-11-17 00:03:38 -0300

    Ultimo Visual Studio

--- src/main.c
+++ src/main.c
@@ -15,16 +15,26 @@
 #define TAG "[MQTT]"
 #define min(a, b) (a>b?b:a)
 static bool mqttOk = false;
-#define LED_GPIO 2
+
 #define id 3
 
-static esp_mqtt_client_handle_t actClient; //puntero a estructura de cliente MQTT
+#define ledPin GPIO_NUM_2
+//configura el led, agrega mascara de bits y configura las resistencias pull up y down 
 
-static void controlarLED(int estado) {
-    gpio_set_level(LED_GPIO, estado);
+void hardware_init(void){
+  gpio_config_t gpioLed =
+  {
+   .pin_bit_mask = 1ULL << ledPin,
+   .mode = GPIO_MODE_DEF_OUTPUT,
+   .pull_up_en = GPIO_PULLUP_DISABLE,
+   .pull_down_en = GPIO_PULLDOWN_DISABLE,
+   .intr_type = GPIO_INTR_DISABLE
+  };   
+  ESP_ERROR_CHECK(gpio_config(&gpioLed));
+ESP_ERROR_CHECK(gpio_set_level(ledPin, 0));
 }
 
-
+static esp_mqtt_client_handle_t actClient; //puntero a estructura de cliente MQTT
 
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
@@ -43,16 +53,17 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         msg_id = esp_mqtt_client_subscribe(client, "/ej02/cmd", 1);
         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-        msg_id = esp_mqtt_client_subscribe(client, "/eje02/3/cmd/", 1);
+        msg_id = esp_mqtt_client_subscribe(client, "/ej02/3/cmd", 1);
         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-        mqttOk=true;
+
+        mqttOk = true;
 break;
     case MQTT_EVENT_DISCONNECTED: 
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
         mqttOk = false;
         break;
 
-     case MQTT_EVENT_SUBSCRIBED:
+    case MQTT_EVENT_SUBSCRIBED:
             ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
             break;
 
@@ -74,51 +85,52 @@ break;
             // Mensaje recibido en /ej02/cmd
             if (strcmp(data, "getid") == 0) {
                 // Publicar el valor de id en /ej02/id
+                ESP_LOGI(TAG, "Received message: %s", data);
+
         msg_id = esp_mqtt_client_publish(client, "/ej02/id", "3", 0, 1, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        ESP_LOGI(TAG, "SE PUBLICO EL MENSAJE REY, msg_id=%d", msg_id);
             }
 
-        } else if (strncmp(topic, "/ej02/", 7) == 0) {
+        } else if (strcmp(topic,"/ej02/3/cmd") == 0) {
             // Mensaje recibido en /ej02/{id}/cmd
-            int receivedId;
-            if (sscanf(topic + 7, "%d/cmd", &receivedId) == 1 && receivedId == id) {
+           
                 // Mensaje dirigido a este dispositivo específico
-                if (strcmp(data, "ledon") == 0) {
-                    // Encender el LED
-                    controlarLED(1);
+                if (strcmp(data, "ledon") == 0 || (strcmp(data, "ledOn")==0)) {
+                     ESP_LOGI(TAG, "LED ON");
+                     ESP_ERROR_CHECK(gpio_set_level(ledPin, 1));
                 } else if (strcmp(data, "ledoff") == 0) {
-                    // Apagar el LED
-                    controlarLED(0);
+                     // Código para apagar el LED
+                     ESP_LOGI(TAG, "LED OFF");  
+                    ESP_ERROR_CHECK(gpio_set_level(ledPin, 0));
+                    
                 }
-            }
+            
         }
         break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-           ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-        }
-        break;
+   case MQTT_EVENT_ERROR:
+    ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+    ESP_LOGE(TAG, "Error type: %d", event->error_handle->error_type);
+    if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+        ESP_LOGE(TAG, "Last errno string: %s", strerror(event->error_handle->esp_transport_sock_errno));
+    }
+    break;
     default:
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
         break;
     }
 }
-void taskPublish(void *param) {
+void taskPublish(void *param){
     char toSend[128];
-    int cont = 1;
-    double frequency = 0.1;  // Frecuencia de la onda senoidal
-    double amplitude = 10.0; // Amplitud de la onda senoidal
-
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-        if (mqttOk) {
-            double value = amplitude * sin(2 * M_PI * frequency * cont);
-            sprintf(toSend, "valor_senoidal: %.2f", value);
-
-            esp_mqtt_client_publish(actClient, "/ej06/senoidal", toSend, 0, 1, 0);
-            cont++;
+    float cont = 1;
+    
+    while(1){
+        vTaskDelay(2500 / portTICK_PERIOD_MS);
+        if(mqttOk){
+            if(cont==2){ESP_ERROR_CHECK(gpio_set_level(ledPin, 1));}
+            float senoidal=sin(cont);
+            sprintf(toSend, "%f", senoidal);
+            esp_mqtt_client_publish(actClient, "/ej02/3/sensor", toSend, 0, 2, 0);
+            cont=cont+(0.2);
         }
     }
 }
@@ -147,6 +159,6 @@ void app_main(void)
     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
-    xTaskCreate(taskPublish, "Publish Task", configMINIMAL_STACK_SIZE + 4096, NULL, 1, NULL);
+    xTaskCreate(taskPublish, "Publish Task", configMINIMAL_STACK_SIZE + 4096, NULL, 1, NULL);
 
 }
